947744914e0cfb3ceab067a1f9814604e190d83d
Patbless1998 <blesstettehe820@gmail.com>
2025-07-06 13:03:27 +0000

    Practice_7

---
+++ practice7.R
@@ -0,0 +1,35 @@
+library(help=knitr)
+?mean
+??anova
+?anova
+1+1
+x<- 1:10
+save.image("bless")
+load("bless")
+cbind(matrix(1:4,2,2),
+      matrix(1:4,2,2))
+ceiling(c(4.999,4.001))
+colMeans(matrix(1:4,2,2))
+cor(c(1,3,5,7),c(4,2,8,6))
+diff(c(2,4,5,8))
+example(cumsum)
+example(mean)
+example("var")
+ls()
+rm(list = ls())
+intersect(c(2,4),c(1,2))
+letters
+LETTERS
+median(c(1,2,3,10))
+rbind(matrix(1:4,2,2),matrix(1:4,2,2))       
+setdiff(1:5,4:8)       
+union(1:5,4:8)
+sum(100.1, 234.9,12.01)
+log(100)
+log10(100)*cos(pi)
+z<-0:100
+z
+replace = TRUE)
+runif(10, min = 5, max = 20)
+sd(data)
+sd(10,20,30,40)
